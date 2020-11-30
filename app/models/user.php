032411d4ae641f3f1b2d8797126382aaa7941522
Patrick Souza do nascimento <psn1462@gmail.com>
2020-11-30 13:38:04 -0300

    Alteração
    update password field

--- app/models/user.php
+++ app/models/user.php
@@ -4,6 +4,6 @@ use \Illuminate\Database\Eloquent\Model;
 
 class User extends Model {
     protected $table = 'users';
-    protected $fillable = ['username','email','pass'];
+    protected $fillable = ['username','email','password'];
 }
 ?>
